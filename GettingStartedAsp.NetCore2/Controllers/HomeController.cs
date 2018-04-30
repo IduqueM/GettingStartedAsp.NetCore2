093a84693786c3b6b257e3d43db5b18c8e54aa5a
Ivan Dario Duque Molina <iduque@ol4b.com>
2018-04-29 23:08:11 -0500

    Commit
    ASP.Net Core 2 project with working MVC endpoint, README, and .gitignore

--- GettingStartedAsp.NetCore2/Controllers/HomeController.cs
+++ GettingStartedAsp.NetCore2/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SampleCosmosCore2App.Core;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,10 +12,39 @@ namespace GettingStartedAsp.NetCore2.Controllers
     [Route("")]
     public class HomeController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        private Persistence _persistence;
+
+        public HomeController(Persistence persistence)
         {
-            return View();
+            _persistence = persistence;
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> IndexAsync()
+        {
+            var samples = await _persistence.GetSamplesAsync();
+            return View("Index", samples);
+        }
+
+        [HttpGet("Create")]
+        public async Task<IActionResult> CreateAsync()
+        {
+            var sample = new Sample() { };
+            return View("Get", sample);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAsync(string id)
+        {
+            var sample = await _persistence.GetSampleAsync(id);
+            return View("Get", sample);
+        }
+
+        [HttpPost()]
+        public async Task<IActionResult> PostAsync([FromForm] Sample sample)
+        {
+            await _persistence.SaveSampleAsync(sample);
+            return RedirectToAction("IndexAsync");
         }
     }
 }
--- GettingStartedAsp.NetCore2/Startup.cs
+++ GettingStartedAsp.NetCore2/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using SampleCosmosCore2App.Core;
 
 namespace GettingStartedAsp.NetCore2
 {
@@ -24,6 +25,12 @@ namespace GettingStartedAsp.NetCore2
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddScoped<Persistence>((s) =>
+            {
+                return new Persistence(
+                    new Uri(Configuration["CosmosDB:URL"]),
+                    Configuration["CosmosDB:PrimaryKey"]);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
